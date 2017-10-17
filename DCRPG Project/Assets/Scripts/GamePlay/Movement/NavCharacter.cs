834bfb5113d34572395bfb1c0c61604123ffdc02
Calum McManus <CalumMcManus7@gmail.com>
2017-10-18 00:50:48 +0100

    *First Push + Added Project + Added Dev Scene for movement + Added basic movement scripts + Added Dev Materials + Changed to Input Manager

---
+++ DCRPG Project/Assets/Scripts/GamePlay/Movement/NavCharacter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+public class NavCharacter : NavMoveable {
+
+    [SerializeField] private Camera m_CharacterCamera;
+    private Ray m_Ray;
+
+    private void Start()
+    {
+        m_Ray = new Ray();
+        if(!m_CharacterCamera)
+        {
+            Debug.LogWarning("NavCharacter: Start: No camera selected, using main!");
+            m_CharacterCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        ClickToMove();
+    }
+
+	private void ClickToMove()
+    {
+        if(Input.GetButtonDown("BasicAttack"))
+        {
+            m_Ray = m_CharacterCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(m_Ray, out hit, Mathf.Infinity))
+            {
+                if(hit.collider.tag == "Ground")
+                {
+                    SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
---
+++ DCRPG Project/Assets/Scripts/GamePlay/Movement/NavMoveable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMoveable : MonoBehaviour
+{
+
+    private NavMeshAgent p_Agent;
+
+    private void Awake()
+    {
+        p_Agent = GetComponent<NavMeshAgent>();
+        if (!p_Agent)
+            Debug.LogError("NavMoveable: Awake: Failed to get NavMeshAgent on this object!");
+    }
+
+    virtual protected void SetDestination(Vector3 coords)
+    {
+        //This will have to calculate a path and move correctly, for now using built in command
+        p_Agent.SetDestination(coords);
+        
+    }
+}
