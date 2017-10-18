7c30254c15662a1441842cdc32a3e8d8cbeacf33
Glue-Stick <harry.burroughs1@hotmail.com>
2017-10-18 15:41:35 +0100

    Harry - Second Push - Menu Start
    Created a menu panel with 4 buttons, buttons do not currently transition when mouse is hovered over it.

---
+++ DCRPG Project/Assets/Scripts/UI/CustomUIObjects/ButtonOveride.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonOveride : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+
+{
+    [SerializeField] private RectTransform m_Target;
+    [SerializeField] private float m_fTargetScale = 1.1f;
+    [SerializeField] private float m_fTransitionSpeed = 1;
+
+    private Vector3 m_StartScale;
+    private bool m_bOverButton = false;  
+    
+    private void Start()
+    {
+        m_StartScale = m_Target.localScale;
+    }  
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_bOverButton = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_bOverButton = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(m_bOverButton)
+        {
+            m_Target.localScale = Vector3.Lerp(m_StartScale, m_StartScale* m_fTargetScale, m_fTransitionSpeed * Time.deltaTime);
+        }
+        else
+        {
+            m_Target.localScale = Vector3.Lerp(m_Target.localScale, m_StartScale, m_fTransitionSpeed * Time.deltaTime);
+        }
+	}
+}
